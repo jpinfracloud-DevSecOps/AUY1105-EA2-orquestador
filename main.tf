b6802d12614cbe989cfd30f026417201d5627ca6
Escael1 <ju.aquebeque@duocuc.cl>
2026-05-29 00:46:25 -0400

    feat: implementar hashes criptograficos inmutables para cumplir CKV_TF_1 de forma nativa

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = var.aws_region
 }
 
-# checkov:skip=CKV_TF_1: Se utiliza versionamiento semantico explicito por diseño de arquitectura para la EVA2.
+# Invocación Remota Inmutable - Módulo de Redes (Equivalente estricto a v0.2.0)
 module "redes" {
-  source = "git::https://github.com/jpinfracloud-DevSecOps/AUY1105-EA2-redes.git?ref=v0.2.0"
+  source = "git::https://github.com/jpinfracloud-DevSecOps/AUY1105-EA2-redes.git?ref=66612180864f4dea43b53237f4cb14b5aa690e36"
 }
 
-# checkov:skip=CKV_TF_1: Se utiliza versionamiento semantico explicito por diseño de arquitectura para la EVA2.
+# Invocación Remota Inmutable - Módulo de Cómputo (Equivalente estricto a v0.2.0)
 module "computo" {
-  source        = "git::https://github.com/jpinfracloud-DevSecOps/AUY1105-EA2-computo.git?ref=v0.2.0"
+  source        = "git::https://github.com/jpinfracloud-DevSecOps/AUY1105-EA2-computo.git?ref=987fde869ac3262e3947537cde25836f7ba372c3"
   project_name  = var.project_name
   instance_type = var.instance_type
   ami_id        = var.ami_id
 
-  # Corrección técnica: Extraemos el primer elemento [0] de la lista 'subnet_ids'
+  # Enlace dinámico de dependencias
   subnet_id = module.redes.subnet_ids[0]
 }
